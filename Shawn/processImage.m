fe2824b49844f37428393eb8e168a8caf634ee27
Shawn Swatek <shawns238@gmail.com>
2015-10-28 19:00:35 -0500

    Added test for adding noise and saving to low quality jpeg

--- Shawn/processImage.m
+++ Shawn/processImage.m
@@ -5,8 +5,30 @@ figure(1);
 image(I);
 % Find most highest points in DCT
 watermarkLength = 1000;
-watermarkScale=0.025;
+watermarkScale=0.05;
 [watermarkedI, watermark]= genApplyWatermark(I,watermarkLength,watermarkScale);
+% Display diff of y
+testMode = 4;
+if testMode == 1
+    watermarkedI = min(max(watermarkedI+0.10*randn(size(I)),0),1);
+elseif testMode == 2
+    % Swap corners of image, distroys watermark if not fixed
+    watermarkedI(:,:,1) = fftshift(watermarkedI(:,:,1));
+    watermarkedI(:,:,2) = fftshift(watermarkedI(:,:,2));
+    watermarkedI(:,:,3) = fftshift(watermarkedI(:,:,3));
+elseif testMode == 3
+    for i = 1:size(I,1)
+        for j = 1:size(I,2)
+            if i<400 && i>370 && j<400 && j>300
+            else
+                watermarkedI(i,j,:) = double(I(i,j,:))/255;
+            end
+        end
+    end
+elseif testMode == 4
+    imwrite(watermarkedI,'tmp.jpg','Quality',7);
+    watermarkedI = imread('tmp.jpg');
+end
 % Display diff of y
 figure(2);
 imagesc(getYComponent(watermarkedI) - getYComponent(I));
