adbd28c24290bb1875290cb3af0677af1f17bbf0
amatl <aimee.matland@gmail.com>
2015-12-07 00:53:31 -0600

    Added Test Code and functions for Audio Watermark

---
+++ Aimee/generateAudioWatermark.m
@@ -0,0 +1,18 @@
+function [ watermark ] = generateAudioWatermark( A_fft, length1 )
+%Want most energy of watermark to lie in low frequencies
+
+%Create array that contains absolute value of A_fft with corresponding
+%index
+temp = zeros(length(A_fft),2);
+for i = 1:length(A_fft)
+    if i~=1
+        value = abs(A_fft(i));
+        temp(i,:) = [value,i];
+    end
+end
+points = sortrows(temp,-1);
+points = points(1:length1,:);
+
+watermark = [points randn(length1,1)];
+end
+
